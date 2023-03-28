3c65fdcd646665feb63d6ef49a83146fef1a268f
AndreyTarshilov <taran55@yadex.ru>
2023-03-28 07:26:27 +0300

    I've tried to create Remote Robot 2..

--- src/RemoteMoveRobot/Main.java
+++ src/RemoteMoveRobot/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         RobotConnectionException mistakeOfConnection = new RobotConnectionException("Another exception");
         RobotConnectionException mistakeSetUpOfConnection = new RobotConnectionException("Another exception", null); // or getCause() instead null
-        /*try { throw RuntimeException anotherException = new IOException("Another exception");
+        /* try { throw RuntimeException anotherException = new IOException("Another exception");
         }
-        catch (Exception e) {}*/
+        catch (Exception e) {} */
     }
 
 }
--- src/RemoteMoveRobot/MoveRobot.java
+++ src/RemoteMoveRobot/MoveRobot.java
@@ -2,12 +2,15 @@ package RemoteMoveRobot;
 
 public class MoveRobot  {
     public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+
         for (int i=0; i<3; i++) {
             robotConnectionManager.getConnection();
 
             if(0 == 0) {
+                // robotConnectionManager (RobotConnection, );
+              /*  @Override
+                void close();*/
                 throw new RobotConnectionException("Another exception");
-                //moveRobotTo();
 
             }
 
